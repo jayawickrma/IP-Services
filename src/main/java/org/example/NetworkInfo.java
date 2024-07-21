16c72132c444d7a3306ddb06847e6be63e5d6ae2
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-07-21 21:45:49 +0530

    complete subnet mask generator and find IP class

--- src/main/java/org/example/NetworkInfo.java
+++ src/main/java/org/example/NetworkInfo.java
@@ -2,10 +2,12 @@ package org.example;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NetworkInfo {
     public static void main(String[] args) {
         try {
+
             InetAddress ip = InetAddress.getLocalHost();
             System.out.println("Current IP address: " + ip.getHostAddress());
             System.out.println("Current Hostname: " + ip.getHostName());
